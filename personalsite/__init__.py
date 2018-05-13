c1b1eb3290b3e4e0a63c93054aac120b442c84ac
Finnbar Keating <finnjkeating@aol.com>
2018-05-13 13:08:47 +0100

    force SSL

--- personalsite/__init__.py
+++ personalsite/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
 from personalsite.secretkey import get_secret_key
+from flask_sslify import SSLify
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -9,6 +10,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = get_secret_key()
 db = SQLAlchemy(app)
 md = Markdown(app)
+sslify = SSLify(app)
 
 import personalsite.views
 import personalsite.database
